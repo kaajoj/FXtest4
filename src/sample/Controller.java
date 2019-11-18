b19e64ce86da4f558b06d8e544fa2beffe7f9cf4
kaajoj <kaajooj@gmail.com>
2019-11-18 03:24:12 +0100

    two views and get data from user

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -17,35 +17,25 @@ import static sample.Calculations.test2;
 
 public class Controller {
 
-    @FXML
-    public Button test1;
+    public int varcount;
+    public int conscount;
 
     @FXML
     public TextArea testTextArea;
 
     @FXML
     public TextField zx1;
-
-    @FXML
     public TextField zx2;
-
-    @FXML
     public TextField ox1;
-
-    @FXML
     public TextField ox2;
-
-    @FXML
     public TextField ox11;
-
-    @FXML
     public TextField ox22;
-
-    @FXML
     public TextField oc1;
+    public TextField oc2;
 
     @FXML
-    public TextField oc2;
+    public TextField countvar;
+    public TextField countcons;
 
     @FXML
     private void testButtonAction(ActionEvent event) {
@@ -59,19 +49,30 @@ public class Controller {
 //            int sum = a+b;
 //            System.out.println(sum);
 //        test();
-        double rzx1 = Double.parseDouble(zx1.getText());
-        double rzx2 = Double.parseDouble(zx2.getText());
-        double rox1 = Double.parseDouble(ox1.getText());
-        double rox2 = Double.parseDouble(ox2.getText());
-        double rox11 = Double.parseDouble(ox11.getText());
-        double rox22 = Double.parseDouble(ox22.getText());
-        double roc1 = Double.parseDouble(oc1.getText());
-        double roc2 = Double.parseDouble(oc2.getText());
-        String res = test2(rzx1, rzx2, rox1, rox2, rox11, rox22, roc1, roc2);
-        testTextArea.setText(res);
+        try {
+            double rzx1 = Double.parseDouble(zx1.getText());
+            double rzx2 = Double.parseDouble(zx2.getText());
+            double rox1 = Double.parseDouble(ox1.getText());
+            double rox2 = Double.parseDouble(ox2.getText());
+            double rox11 = Double.parseDouble(ox11.getText());
+            double rox22 = Double.parseDouble(ox22.getText());
+            double roc1 = Double.parseDouble(oc1.getText());
+            double roc2 = Double.parseDouble(oc2.getText());
+            String res = test2(rzx1, rzx2, rox1, rox2, rox11, rox22, roc1, roc2);
+            testTextArea.setText(res);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getUserData(){
+        varcount = Integer.parseInt(countvar.getText());
+        conscount = Integer.parseInt(countcons.getText());
+        System.out.println(varcount + " " + conscount);
     }
 
     public void changeView(ActionEvent event) throws IOException {
+        getUserData();
         Parent viewParent = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene viewScene = new Scene(viewParent);
 
